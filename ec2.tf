46ef1e2beaf849f0c0512cdaca59e96f0a0b75a9
Rishitha yeruva <rishithayeruva@Rishithas-MacBook-Pro.local>
2024-08-27 12:35:03 +0530

    test

--- ec2.tf
+++ ec2.tf
@@ -2,5 +2,12 @@ resource "aws_instance" "rishi" {
   ami                     = "ami-06d64e8645ed82fd0"
   instance_type           = "m5.large"
   subnet_id               = aws_subnet.main.id
-
+  count                   = var.create_instance ? 1 : 0
+  tags = {
+      Name = local.Instance_Name,
+      username = var.Name1,
+      Location = local.Instance_location
+      added_by = var.Name
+    }
+  
 }
